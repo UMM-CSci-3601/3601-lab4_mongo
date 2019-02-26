7cf84a2c38e896de635bfec3e34cef2464d9efba
Adam Schug <schug047@morris.umn.edu>
2019-02-26 14:06:58 -0600

    Creates a new java class for todos in the todo package called TodoController.java

---
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,87 @@
+package umm3601.todo;
+
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class TodoController {
+  private final MongoCollection<Document> todoCollection;
+
+
+  public TodoController(MongoDatabase database) {
+    todoCollection = database.getCollection("todos");
+  }
+
+  public String getTodo(String id) {
+    FindIterable<Document> jsonTodos
+      = todoCollection
+      .find(eq("_id", new ObjectId(id)));
+
+    Iterator<Document> iterator = jsonTodos.iterator();
+    if (iterator.hasNext()) {
+      Document user = iterator.next();
+      return user.toJson();
+    } else {
+      // We didn't find the desired todo
+      return null;
+    }
+  }
+
+  public String getTodos(Map<String, String[]> queryParams) {
+
+    Document filterDoc = new Document();
+
+    if (queryParams.containsKey("status")) {
+      boolean targetStatus = Boolean.getBoolean(queryParams.get("status")[0]);
+      filterDoc = filterDoc.append("status", targetStatus);
+    }
+
+//    if (queryParams.containsKey("company")) {
+//      String targetContent = (queryParams.get("company")[0]);
+//      Document contentRegQuery = new Document();
+//      contentRegQuery.append("$regex", targetContent);
+//      contentRegQuery.append("$options", "i");
+//      filterDoc = filterDoc.append("company", contentRegQuery);
+//    }
+
+    //FindIterable comes from mongo, Document comes from Gson
+    FindIterable<Document> matchingTodos = todoCollection.find(filterDoc);
+
+    return serializeIterable(matchingTodos);
+  }
+
+  private String serializeIterable(Iterable<Document> documents) {
+    return StreamSupport.stream(documents.spliterator(), false)
+      .map(Document::toJson)
+      .collect(Collectors.joining(", ", "[", "]"));
+  }
+
+  public String addNewTodo(String owner, boolean status, String category, String body) {
+
+    Document newTodo = new Document();
+    newTodo.append("owner", owner);
+    newTodo.append("status", status);
+    newTodo.append("category", category);
+    newTodo.append("body", body);
+
+    try {
+      todoCollection.insertOne(newTodo);
+      ObjectId id = newTodo.getObjectId("_id");
+      System.err.println("Successfully added new user [_id=" + id + ", owner=" + owner + ", status=" + status + " category=" + category + " body=" + body + ']');
+      return id.toHexString();
+    } catch (MongoException me) {
+      me.printStackTrace();
+      return null;
+    }
+  }
+}
