782147f52470b7bccc6d9dbdbc75bf86f1d2ff14
Adam Schug <schug047@morris.umn.edu>
2019-02-26 14:18:28 -0600

    Updates server to allow todo endpoints to be requested providing server functionality.

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -6,21 +6,27 @@ import spark.Request;
 import spark.Response;
 import umm3601.user.UserController;
 import umm3601.user.UserRequestHandler;
+import umm3601.todo.TodoController;
+import umm3601.todo.TodoRequestHandler;
 
 import static spark.Spark.*;
 import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Server {
   private static final String userDatabaseName = "dev";
+  private static final String todoDatabaseName = "dev2";
   private static final int serverPort = 4567;
 
   public static void main(String[] args) {
 
     MongoClient mongoClient = new MongoClient();
     MongoDatabase userDatabase = mongoClient.getDatabase(userDatabaseName);
+    MongoDatabase todoDatabase = mongoClient.getDatabase(todoDatabaseName);
 
     UserController userController = new UserController(userDatabase);
     UserRequestHandler userRequestHandler = new UserRequestHandler(userController);
+    TodoController todoController = new TodoController(todoDatabase);
+    TodoRequestHandler todoRequestHandler = new TodoRequestHandler(todoController);
 
     //Configure Spark
     port(serverPort);
@@ -63,6 +69,12 @@ public class Server {
     get("api/users", userRequestHandler::getUsers);
     get("api/users/:id", userRequestHandler::getUserJSON);
     post("api/users/new", userRequestHandler::addNewUser);
+
+    //List todos, filtered using query parameters
+    get("api/todos", todoRequestHandler::getTodos);
+    get("api/todos/:id", todoRequestHandler::getTodoJSON);
+    get("api/todos/new", todoRequestHandler::addNewTodo);
+
 
     // An example of throwing an unhandled exception so you can see how the
     // Java Spark debugger displays errors like this.
